0ea3c68a1c237b14955018117800212c956dbdf7
hasanali207 <hasanali142228@gmail.com>
2024-10-19 12:55:00 +0600

    added data

---
+++ src/components/Banner.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+export default function Banner() {
+  return (
+    
+
+    <Carousel>
+    <div>
+        <img src="assets/1.jpeg" />
+        <p className="legend">Legend 1</p>
+    </div>
+    
+    <div>
+        <img src="assets/3.jpeg" />
+        <p className="legend">Legend 3</p>
+    </div>
+</Carousel>
+  )
+}
